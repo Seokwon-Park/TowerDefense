b4696d00d73f020af4d2135f702ceb35f1bad985
SeokwonPark <rty3394@naver.com>
2023-08-01 16:28:08 +0900

    E10
    TD Tutorial

---
+++ TowerDefenseTutorial/Assets/Scenes/Scripts/BuildManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one BUildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+    public GameObject missileLauncherPrefab;
+
+    private GameObject turretToBuild;
+    
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+    public void SetTurretToBuild(GameObject turret)
+    {
+        turretToBuild = turret;
+    }
+}
